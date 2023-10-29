9c4fb9390d1d092c63147b6c16233fe7c59221ed
icachintyas <aicachintya@gmail.com>
2023-10-29 15:36:47 +0700

    Mengisi Database step4

--- app/src/main/java/com/example/roomwordsample/WordsApplication.kt
+++ app/src/main/java/com/example/roomwordsample/WordsApplication.kt
@@ -1,6 +1,9 @@
 package com.example.roomwordsample
 
 import android.app.Application
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
@@ -14,30 +17,4 @@ class WordsApplication : Application() {
     // rather than when the application starts
     val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
-}
-private class WordDatabaseCallback(
-    private val scope: CoroutineScope
-) : RoomDatabase.Callback() {
-
-    override fun onCreate(db: SupportSQLiteDatabase) {
-        super.onCreate(db)
-        INSTANCE?.let { database ->
-            scope.launch {
-                populateDatabase(database.wordDao())
-            }
-        }
-    }
-
-    suspend fun populateDatabase(wordDao: WordDao) {
-        // Delete all content here.
-        wordDao.deleteAll()
-
-        // Add sample words.
-        var word = Word("Hello")
-        wordDao.insert(word)
-        word = Word("World!")
-        wordDao.insert(word)
-
-        // TODO: Add your own words!
-    }
 }
