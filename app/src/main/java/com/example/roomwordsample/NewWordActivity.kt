179c1b23f7a7148b853b10f6fcd572eff339a788
icachintyas <aicachintya@gmail.com>
2023-10-29 16:30:28 +0700

    Menambahakan NewWordActivity Step6

---
+++ app/src/main/java/com/example/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,16 @@
+package com.example.roomwordsample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+
+class NewWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
