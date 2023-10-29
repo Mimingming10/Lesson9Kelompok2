9b31188d7d9ffd7d7d29fd57826bfeedaa91a78e
icachintyas <aicachintya@gmail.com>
2023-10-29 17:14:47 +0700

    Terhubung dengan data step2

--- app/src/main/java/com/example/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.roomwordsample
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }}
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
     }
-}
-private val wordViewModel: WordViewModel by viewModels {
-    WordViewModelFactory((application as WordsApplication).repository)
-}
+
+wordViewModel.allWords.observe(this, Observer { words ->
+    // Update the cached copy of the words in the adapter.
+    words?.let { adapter.submitList(it) }
+
+
+
